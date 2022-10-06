fba5e2bfe92a1a696b80a92a853191a0fbf85465
zloy_tulen <zloy_tulen_1000@proton.me>
2022-10-06 13:41:59 +0700

    Add PIVT chunk

--- model-mdx/src/types/chunk/pivt.rs
+++ model-mdx/src/types/chunk/pivt.rs
@@ -1,11 +1,15 @@
-use super::Chunk;
+use super::utils::Tag;
+use super::*;
 use crate::encoder::error::Error as EncodeError;
 use crate::parser::Parser;
-use crate::types::materialize::Materialized;
-use super::utils::Tag;
+use crate::types::materialize::*;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Pivt {}
+pub const PIVT_SIZE: usize = 12;
+
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct Pivt {
+    pub points: Vec<[f32; 3]>,
+}
 
 impl Chunk for Pivt {
     fn tag() -> Tag {
@@ -17,10 +21,11 @@ impl Materialized for Pivt {
     type Version = u32;
 
     fn parse_versioned(_: Option<Self::Version>, input: &[u8]) -> Parser<Self> {
-        unimplemented!();
+        let (input, points) = parse_fixed_elements_chunk::<PIVT_SIZE, Pivt, _>(input)?;
+        Ok((input, Pivt { points }))
     }
 
     fn encode(&self, output: &mut Vec<u8>) -> Result<(), EncodeError> {
-        unimplemented!();
+        encode_chunk::<Seqs, _>(|output| encode_fixed_vec(&self.points)(output))(output)
     }
 }
